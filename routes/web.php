e5a1fa5ecaf2d0277d1577076f62042b87e9724d
khursheed <khursheedafridi78@gmail.com>
2024-11-26 15:20:58 +0500

    second commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('practice',function(){
+    return view('laravel-exceptions-renderer::components.card');
+    });
